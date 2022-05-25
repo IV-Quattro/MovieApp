1a124c88b1a1a28d73867f39878c293e9c668069
Ivan Reggiani <ivanreggiani1997@gmail.com>
2022-05-25 11:05:06 +0200

    fetch + debug

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
- 
+ const URL_ONION = "https://www.omdbapi.com/?apikey=450e73c0&s=onion+movie";
+
+ export const onion = () => {
+     fetch(URL_ONION)
+     .then(jsonResponse => jsonResponse.json())
+     .then(objResult => {
+        const movie = objResult.Search;
+        console.log(movie);
+     });
+ }
