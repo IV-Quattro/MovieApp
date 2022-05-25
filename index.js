73b27b10a6a56a929a659ff3e436bf66db4d60c3
Ivan Reggiani <ivanreggiani1997@gmail.com>
2022-05-25 12:02:06 +0200

    aggiunti nightmare movies e watchman series

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
  const URL_ONION = "https://www.omdbapi.com/?apikey=450e73c0&s=onion+movie";
+ const URL_NIGHTMARE_MOVIES = "https://www.omdbapi.com/?apikey=450e73c0&s=nightmare&type=movie";
+ const URL_WATCHMAN_SERIES = "https://www.omdbapi.com/?apikey=450e73c0&s=watchman&type=series";
 
  export const onion = () => {
      fetch(URL_ONION)
      .then(jsonResponse => jsonResponse.json())
      .then(objResult => {
         const movie = objResult.Search;
+        console.log("stampa the onion movie");
         console.log(movie);
      });
  }
+
+ export const nightmare = () => {
+     fetch(URL_NIGHTMARE_MOVIES)
+     .then(response => response.json())
+     .then(result => {
+         const movies = result.Search;
+         console.log("stampa movies nightmare");
+         console.log(movies);
+     });
+ }
+
+ export const watchman = () => {
+    fetch(URL_WATCHMAN_SERIES)
+    .then(response => response.json())
+    .then(result => {
+        const series = result.Search;
+        console.log("stampa series watchman");
+        console.log(series);
+    });
+}
