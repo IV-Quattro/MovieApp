788449a1537abfbaf29b8df92c113f61f4b19ed1
Ivan Reggiani <ivanreggiani1997@gmail.com>
2022-06-01 18:02:50 +0200

    push fine lezione, tutto da correggere

--- index.js
+++ index.js
@@ -16,22 +16,69 @@ export const apiList = (s,type) => {
         .then(jsonResponse => jsonResponse.json())
         .then(objResult => {
            const items = objResult.Search;
-           delay();
+         
            viewItems(items);
-           console.log(items);
+           
         });
    }
    
+  
 
 
  const viewItems = (items) => {
-     
-    items.map((item) => {
-        console.group(item.Title);
-            console.log(item.Year);
-            console.log(item.Type);
-            console.log(item.Poster);
-        console.groupEnd();
+    const place = document.getElementById("all");
+    items.map((film) => {
+        //place.appendChild(createHTMLfilm(film));
+        place.appendChild(createFilmTitle(film));
+        place.appendChild(createFilmPoster(film));
+        place.appendChild(createFilmYear(film));
     });
-
    }
+   
+
+ /*ritorna un pacchetto con tutte le info insieme separate da uno spazio
+   const createHTMLfilm = (film) => {
+       let parametro = [film.Title, film.Year, film.imdbID, film.Type, film.Poster];
+       const tutto = document.createElement("div");
+       for(let i=0;i<parametro.length; i++){
+            const paragrafo = document.createElement("p");
+            const nodo = document.createTextNode(parametro[i]);
+            paragrafo.appendChild(nodo);
+            tutto.appendChild(paragrafo);
+       }
+        const spazio = document.createElement("br");
+        tutto.appendChild(spazio);
+        return tutto;
+    }*/
+
+    const createFilmPoster = (film) => {
+        const paragrafo = document.createElement("p");
+        const nodo = document.createTextNode(film.Poster);
+        paragrafo.appendChild(nodo);
+        return paragrafo;
+    }
+
+    const createFilmTitle = (film) => {
+        const paragrafo = document.createElement("p");
+        const nodo = document.createTextNode(film.Title);
+        paragrafo.appendChild(nodo);
+        return paragrafo;
+    }
+    const createFilmYear = (film) => {
+        const paragrafo = document.createElement("p");
+        const nodo = document.createTextNode(film.Year);
+        paragrafo.appendChild(nodo);
+        return paragrafo;
+    }
+
+    const createHTMLcard = (film) => {
+        const riga = document.createElement("div");
+            riga.class = "row";
+        const colonna = document.createElement("div");
+        colonna.class = "col"
+        riga.appendChild(colonna);
+        riga.appendChild(colonna);
+        riga.appendChild(colonna);
+        const carta = document.createElement("div.card")
+
+    }
