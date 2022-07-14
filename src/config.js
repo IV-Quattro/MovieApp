f096fdf231e1840a03134be6c09deaca4575409f
IV-Quattro <ivanreggiani1997@gmail.com>
2022-07-14 21:20:15 +0200

    commentati i files rimanenti tolte alcune immagini, scelte altre per il carusel, eliminato un file

--- src/config.js
+++ src/config.js
@@ -1,8 +1,11 @@
-export const API_KEY="450e73c0";
+//api key a mio nome
+export const API_KEY="450e73c0"; 
+//assemblaggio url per chiamate ajax
 export const URL_BASE=`https://www.omdbapi.com/?apikey=${API_KEY}&`;
 
 
-
+//per non sbagliare la sintassi
+//me li suggerisce
 export const CONTENT_TYPE = {
     MOVIES: "movie",
 	SERIES: "series",
